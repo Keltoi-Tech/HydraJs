f906ef4dea53dca524bd0e69756858a6846a2c4d
DaniloHenrique <keltoitech@gmail.com>
2025-05-01 18:58:07 -0300

    feat.: including create on db repository

--- src/repository/db/index.js
+++ src/repository/db/index.js
@@ -33,32 +33,40 @@ export default class Repository{
 
     set context(value=knex()){ this.myContext=()=>value(this.#name) }
 
-    insert=(model=new Model())=>
+    create = (model=new Model())=>
+        this.myContext()
+        .insert({
+            ...model.key, 
+            ...model.entity
+        })
+        .then(()=>model)
+
+    insert = (model=new Model())=>
         this.myContext()
         .insert(model.entity,Object.keys(model.key))
         .then(ids=>model.key = ids[0])
         .then(()=>model)
 
-    update=(model=new Model())=>
+    update = (model=new Model())=>
         this.myContext()
         .where(model.key)
         .update(model.entity)
         .then(affected=>affected > 0)
 
-    delete=(key={})=>
+    delete = (key={})=>
         this.myContext()
         .where(key)
         .del()
         .then(affected=>affected > 0)
 
-    get=(key={})=>
+    get = (key={})=>
         this.myContext()
        .where(key)
        .first()
        .then(model=>Repository.anyOrError(model,{code:404,message:'Not found'}))
        .then(this.modelInstance) 
 
-    list=()=>
+    list = ()=>
         this.myContext()
         .select()
         .then(result=>Repository.setOrEmpty(result,this.modelInstance))
